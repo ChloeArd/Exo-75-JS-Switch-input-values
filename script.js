2d9d8fcf74688b46b56a7bc565a4336f43ca2cba
ChloeArd <chloe.ardoise@gmail.com>
2020-12-04 12:52:14 +0100

    Exercice 75

---
+++ script.js
@@ -0,0 +1,9 @@
+let input1 = document.getElementById('input1');
+let input2 = document.getElementById('input2');
+let button = document.getElementById('switch');
+
+function permuter() {
+    input1.innerHTML = prompt();
+
+
+}
