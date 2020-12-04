9efe70e5dbadc0bbcaf6367e879662d1443014e5
ChloeArd <chloe.ardoise@gmail.com>
2020-12-04 16:23:12 +0100

    Exercice 75

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let input1 = document.getElementById('input1');
-let input2 = document.getElementById('input2');
 let button = document.getElementById('switch');
 
 function permuter() {
-    input1.innerHTML = prompt();
+    let input1 = document.getElementById('input1').value;
+    console.log("Vous avez saisi " + input1);
 
+    let input2 = document.getElementById('input2').value;
+    console.log("Vous avez saisi " + input2);
 
+    input1.value = input2.value;
 }
+
+button.addEventListener("click", permuter);
